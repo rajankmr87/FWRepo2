b770dab684a42f91afafbab5a0e372b2d6663402
rajankmr87 <rajan.kmr.87@gmail.com>
2025-04-22 20:14:09 +0530

    BaseClass Line76,77,78 updated

--- AdvanceSeleniumFrameWork2/src/main/java/Generic_Utility/BaseClass.java
+++ AdvanceSeleniumFrameWork2/src/main/java/Generic_Utility/BaseClass.java
@@ -68,14 +68,14 @@ public class BaseClass {
 
 		wlib.windowMaximize(driver);
 
-		String URL = file.getKeyAndValuePair("url");
-		String USERNAME = file.getKeyAndValuePair("username");
-		String PASSWORD = file.getKeyAndValuePair("password");
+//		String URL = file.getKeyAndValuePair("url");
+//		String USERNAME = file.getKeyAndValuePair("username");
+//		String PASSWORD = file.getKeyAndValuePair("password");
 
-		//reading url,username,password values from cmd line
-//		String URL = System.getProperty("url");
-//	    String USERNAME = System.getProperty("username");
-//		String PASSWORD = System.getProperty("password");
+		//reading url,username,password values from cmd  / JENKINS
+		String URL = System.getProperty("url");
+	    String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
 		
 		driver.get(URL);
 
